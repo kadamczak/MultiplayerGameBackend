4eb5a36982f0df0aae264c9253bfcba264b9aa50
kadamczak <kadamczak.dev@gmail.com>
2025-10-22 16:50:42 +0200

    Setup logging

--- src/MultiplayerGameBackend.API/Extensions/WebApplicationBuilderExtensions.cs
+++ src/MultiplayerGameBackend.API/Extensions/WebApplicationBuilderExtensions.cs
@@ -1,6 +1,16 @@
+using Serilog;
+
 namespace MultiplayerGameBackend.API.Extensions;
 
-public class WebApplicationBuilderExtensions
+public static class WebApplicationBuilderExtensions
 {
-    
+    public static void AddPresentation(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddAuthentication();
+        builder.Services.AddControllers();
+        
+        builder.Host.UseSerilog((context, configuration) =>
+            configuration.ReadFrom.Configuration(context.Configuration)
+        );
+    }
 }
--- src/MultiplayerGameBackend.API/Program.cs
+++ src/MultiplayerGameBackend.API/Program.cs
@@ -1,23 +1,32 @@
-var builder = WebApplication.CreateBuilder(args);
+using MultiplayerGameBackend.API.Extensions;
+using Serilog;
 
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+try
 {
-    app.MapOpenApi();
+    var builder = WebApplication.CreateBuilder(args);
+    builder.AddPresentation();
+
+
+    var app = builder.Build();
+
+    app.UseSerilogRequestLogging();
+    
+    if (app.Environment.IsDevelopment())
+    {
+        app.MapOpenApi();
+    }
+    app.UseHttpsRedirection();
+    app.UseAuthorization();
+    app.MapControllers();
+    app.Run();
+}
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Application startup failed.");
+}
+finally
+{
+    Log.CloseAndFlush();
 }
 
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
+public partial class Program { }
