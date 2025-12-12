634fd386fa3cd335494ca2b3207abf7ee1b7ac9e
kadamczak <kadamczak.dev@gmail.com>
2025-12-12 14:16:47 +0100

    Move UserContext usage from UserService to UserController

--- src/MultiplayerGameBackend.Application/Users/IUserService.cs
+++ src/MultiplayerGameBackend.Application/Users/IUserService.cs
@@ -5,10 +5,10 @@ namespace MultiplayerGameBackend.Application.Users;
 
 public interface IUserService
 {
-    Task AssignUserRole(Guid id, ModifyUserRoleDto dto, CancellationToken cancellationToken);
-    Task UnassignUserRole(Guid id, ModifyUserRoleDto dto, CancellationToken cancellationToken);
-    Task<UserGameInfoDto> GetCurrentUserGameInfo(bool includeCustomization, bool includeUserItems, CancellationToken cancellationToken);
-    Task UpdateUserAppearance(UpdateUserAppearanceDto dto, CancellationToken cancellationToken);
-    Task<string> UploadProfilePicture(Stream imageStream, string fileName, CancellationToken cancellationToken);
-    Task DeleteProfilePicture(CancellationToken cancellationToken);
+    Task AssignUserRole(Guid userId, ModifyUserRoleDto dto, CancellationToken cancellationToken);
+    Task UnassignUserRole(Guid userId, ModifyUserRoleDto dto, CancellationToken cancellationToken);
+    Task<UserGameInfoDto> GetCurrentUserGameInfo(Guid userId, bool includeCustomization, bool includeUserItems, CancellationToken cancellationToken);
+    Task UpdateUserAppearance(Guid userId, UpdateUserAppearanceDto dto, CancellationToken cancellationToken);
+    Task<string> UploadProfilePicture(Guid userId, Stream imageStream, string fileName, CancellationToken cancellationToken);
+    Task DeleteProfilePicture(Guid userId, CancellationToken cancellationToken);
 }
